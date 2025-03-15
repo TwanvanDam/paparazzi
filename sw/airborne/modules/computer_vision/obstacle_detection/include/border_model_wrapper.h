2f93c15bb50db6b765ca762c1a08c8b21d5fbbee
Daniel Jonathan Rugge <35196043+TheOrange-cmd@users.noreply.github.com>
2025-03-15 14:15:43 +0100

    Debugging border model

--- sw/airborne/modules/computer_vision/obstacle_detection/include/border_model_wrapper.h
+++ sw/airborne/modules/computer_vision/obstacle_detection/include/border_model_wrapper.h
@@ -2,13 +2,17 @@
 #ifndef BORDER_MODEL_WRAPPER_H
 #define BORDER_MODEL_WRAPPER_H
 
-// First declare the original entry function signature
-void entry(const float tensor_input[1][3][240][240], float tensor_output[1][1]);
-
-// Then declare our wrapper function that will call it
+// Declare the entry function
 void entry_border(const float tensor_input[1][3][240][240], float tensor_output[1][1]);
 
-void clear_border_tensors(void);
-void print_tensor_debug_info(const float* input_tensor, const float* output_tensor);
+enum DebugLevel {
+  DEBUG_NONE = 0,
+  DEBUG_MINIMAL = 1,   // Just final outputs
+  DEBUG_MODERATE = 2,  // Key layer statistics
+  DEBUG_VERBOSE = 3    // All layer details
+};
 
-#endif
+extern int frame_counter;
+extern enum DebugLevel current_debug_level;
+
+#endif // BORDER_MODEL_WRAPPER_H
--- sw/airborne/modules/computer_vision/obstacle_detection/include/obstacle_detection.h
+++ sw/airborne/modules/computer_vision/obstacle_detection/include/obstacle_detection.h
@@ -1,11 +1,12 @@
+// obstacle_detection.h
 #ifndef OBSTACLE_DETECTION_H
 #define OBSTACLE_DETECTION_H
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <sys/time.h>  
+#include <sys/time.h>
 #include "modules/computer_vision/lib/vision/image.h"
-#include "video_stream.h"  
+#include "video_stream.h"
 
 #define MODEL_TYPE_OBSTACLE 0
 #define MODEL_TYPE_BORDER 1
@@ -16,6 +17,12 @@
 #define BOTTOM_CAMERA_WIDTH 240
 #define BOTTOM_CAMERA_HEIGHT 240
 
+#define DEBUG_LEVEL_NONE 0
+#define DEBUG_LEVEL_BASIC 1
+#define DEBUG_LEVEL_VERBOSE 2
+
+extern int debug_level;  // Global debug level
+
 // Configuration and state
 struct obstacle_detection_t {
     bool front_enabled;
@@ -25,20 +32,51 @@ struct obstacle_detection_t {
 
 // Shared data for each camera
 struct camera_data_t {
+    // Existing members
+    struct image_t *frame;
     bool frame_ready;
-    struct image_t* frame;
-    uint32_t frames_received;
-    uint32_t frames_processed;
-    struct timeval last_frame_received_time;
-    struct timeval last_frame_processed_time;
-    float received_fps;
-    float processed_fps;
-    struct stream_context_t stream_ctx;
-    pthread_mutex_t frame_mutex;
-    
-    // Add RGB buffer members
-    float* rgb_buffer;
+    float *rgb_buffer;
     size_t rgb_buffer_size;
+    pthread_mutex_t frame_mutex;
+    struct stream_context_t stream_ctx;
+    bool initialized;
+    
+    // Thread tracking
+    pthread_t last_callback_thread;
+    pthread_t last_inference_thread;
+    uint32_t callback_count;
+    uint32_t inference_count;
+    
+    // Memory tracking
+    void* last_frame_addr;
+    void* last_rgb_buffer_addr;
+    size_t last_frame_size;
+    uint32_t rgb_buffer_checksum;
+    uint32_t error_count;
+    size_t rgb_buffer_actual_size;  // Track actual allocated size
+    void* rgb_buffer_last_write;    // Track last write position
+    
+    // Timing tracking
+    struct timespec last_callback_time;
+    struct timespec last_inference_time;
+    double avg_processing_time;
+    double max_processing_time;
+    
+    // Statistics
+    uint32_t frames_processed;
+    uint32_t frames_received;
+    uint32_t last_successful_inference;
+    float last_inference_result;
+    float min_inference_result;
+    float max_inference_result;
+    float sum_inference_results;
+    uint32_t significant_changes;
+    uint32_t summary_counter;
+    
+    // Frame timing
+    struct timeval last_frame_received_time;
+    float received_fps;
+    float input_tensor[1][3][240][240];
 };
 
 // External declarations
@@ -46,7 +84,12 @@ extern struct obstacle_detection_t obstacle_detection;
 extern struct camera_data_t front_camera_data;
 extern struct camera_data_t bottom_camera_data;
 
-// Function declarations
+// Debug functions
+void calculate_checksum(const void* data, size_t size, uint32_t* checksum);
+void validate_buffers(struct camera_data_t* data, const char* location);
+void log_state(struct camera_data_t* data, const char* location);
+
+// Main interface
 bool obstacle_detection_init(void);
 void obstacle_detection_periodic(void);
 void obstacle_detection_cleanup(void);
